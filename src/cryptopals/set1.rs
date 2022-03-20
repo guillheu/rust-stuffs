cf036d32fc39bd36c894887205f230416cc755b3
guillheu <guillheu@gmail.com>
2022-03-20 21:33:51 +0100

    update

--- src/cryptopals/set1.rs
+++ src/cryptopals/set1.rs
@@ -1,23 +1,26 @@
-extern crate base64;
-
+extern crate clap;
 
 pub fn challenge_1(){
-    println!("Please enter hex string :");
-    let input_stream = std::io::stdin();            //getting stdin stream
+    println!("Please enter hex string :");        //getting stdin stream
     let mut line = String::new();
-    input_stream.read_line(&mut line).unwrap();     //reading a line : .unwrap() THIS WILL ENFORCE 8 BITS UTF-8 (thats a good thing, but I'd like to know more about this function)
+    std::io::stdin().read_line(&mut line).expect("error : unable to read user input");     //reading a line : .unwrap() THIS WILL ENFORCE 8 BITS UTF-8 (thats a good thing, but I'd like to know more about this function)
     line = line.trim().to_string();                 //trimming the line (contains a trailing \n)
-    let b64 = hex_string_to_b64(&line);             //converting given string (expectedly hex string) into base64
-
+    let b64 = match hex_string_to_b64(&line){       //converting given string (expectedly hex string) into base64
+        Ok(r)  => r,
+        Err(e) => {
+            eprintln!("Error when converting hex string to base 64 : {}", e);
+            "".to_string()
+        }
+    };
     println!("{}", b64);
 }
 
 
 
-pub fn hex_string_to_b64(s: &String) -> String{
-    let hex:Vec<u8> = hex_string_to_bytes(s).unwrap();       //converting given hex string into byte equivalents. e.g : "aa" would be x'aa', or b'10101010', or 170_u8
-    let b64:String = b64_encode(hex);                        //encoding bytes into base64 string
-    b64
+pub fn hex_string_to_b64(s: &String) -> Result<String, String>{
+    let hex:Vec<u8> = hex_string_to_bytes(s)?;       //converting given hex string into byte equivalents. e.g : "aa" would be x'aa', or b'10101010', or 170_u8
+    let b64:String = b64_encode(&hex);                        //encoding bytes into base64 string
+    Ok(b64)
 }
 
 
@@ -32,38 +35,24 @@ pub fn hex_string_to_bytes(hex: &String) -> Result<Vec<u8>, String>{
         //And every 2 hex character encodes a byte
         //So the length of the byte vector should be half that of the hex string
         //Thus, so should its capacity if we want to optimise memory usage
-        return Err("Invalid hex string.".to_string());
+        return Err("Hex string should have an even amount of characters".to_string());
     }
     let mut r:Vec<u8> = Vec::with_capacity(hex.len()/2);
     for byte_hex in hex.chars().collect::<Vec<char>>().windows(2).step_by(2) {
-        r.push(make_byte_from_hex(byte_hex[0], byte_hex[1]));          //second byte => 4 least signifiant bits. leave them in the least significant half. merge (bitwise OR) with other half.
+        r.push(make_byte_from_hex(byte_hex[0], byte_hex[1])?);          //second byte => 4 least signifiant bits. leave them in the least significant half. merge (bitwise OR) with other half.
     }
     Ok(r)
 }
 
-pub fn make_byte_from_hex(hex1: char, hex2: char) -> u8 {
-    ((hex1.to_digit(16).unwrap() as u8) << 4) | (hex2.to_digit(16).unwrap() as u8)
+pub fn make_byte_from_hex(hex1: char, hex2: char) -> Result<u8, String> {
+    let msb = (hex1
+        .to_digit(16)
+        .ok_or("not a hex digit")? as u8) << 4;
+    let lsb = hex2
+        .to_digit(16)
+        .ok_or("not a hex digit")? as u8;
+    Ok(msb | lsb)
 }
-
-//equivalent to char::to_digit(16)
-//except to_digit will actually check if the character is hexadecimal
-//this function will accept a character that's over 1 byte long if its least significant byte has the same value as '0123456789', 'abcdef' of 'ABCDEF'
-//for instance, this function will think '〱' is a '1' because its lsb has value 49, which is '1' : https://www.w3schools.com/charsets/ref_utf_basic_latin.asp
-fn match_hex_val_to_bits(hex_char: char) -> Result<u8, String> {
-    println!("{}", hex_char as u8);
-    let shift = match hex_char as u8 {
-        48..=57     => Some(48),
-        97..=102    => Some(87),
-        65..=70     => Some(55),
-        _           => None,
-    };
-    println!("shift = {}", shift.unwrap());
-    match shift {
-        Some(n) => Ok(hex_char as u8 - n),
-        None    => Err(format!("Character {} is not hexadecimal", hex_char))
-    }
-}
-
 
 
 
@@ -74,7 +63,7 @@ fn match_hex_val_to_bits(hex_char: char) -> Result<u8, String> {
 
 
 
-pub fn b64_encode(bytes: Vec<u8>) -> String {
+pub fn b64_encode(bytes: &[u8]) -> String {
     let b64_word_count = (((bytes.len() as f64)/3.) * 4.).ceil() as usize;
 
     let mut b64_words:Vec<u8> = Vec::with_capacity(b64_word_count);
@@ -85,7 +74,7 @@ pub fn b64_encode(bytes: Vec<u8>) -> String {
         let start_bit = (i*6)%24;
         let end_bit = start_bit + 5;
         let b1:u8 = bytes[(i/4)*3 + start_bit/8];
-        let mut b2:u8;
+        let b2:u8;
         if bytes.len() > ((i/4)*3 + end_bit/8) {
             b2 = bytes[(i/4)*3 + end_bit/8];
         }
@@ -109,7 +98,7 @@ pub fn b64_encode(bytes: Vec<u8>) -> String {
         out.push(b64_word_to_char(word).unwrap());
     }
 
-    for i in 0..padding {
+    for _i in 0..padding {
         out.push('=');
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 mod cryptopals;
 fn main() {
-
-
     cryptopals::set1::challenge_1();
-
-    // let b64_char = cryptopals::set1::make_b64_char(byte1, byte2, 0u8);
-    // println!("{}", b64_char);
 }
